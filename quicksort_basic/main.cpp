dbac151587646de8f57a28fe350950262f0e742e
dmac <dmac@nospam.com>
2020-09-24 17:45:42 +1000

    real quicksort added

--- quicksort_basic/main.cpp
+++ quicksort_basic/main.cpp
@@ -6,27 +6,105 @@
 #include "Timer.h"
 using namespace std;
 
-bool quicksort(int* numbers, int left, int right)
-{
-  // Choose right as starting pivot
-  int pivot = right;
-  // Is left bigger than pivot?
-  for (int i = 0; i < right; i++)
-  {
-	  numbers[i] = sqrt(numbers[i]);
-  }
+// Selects a pivot int in the provided range and places 
+// lower ints to left, higher ints to right.
+int choosePivotAndPartition(int* numbers, int left, int right);
 
-  cout << "\t Doing 100K sqrts\n";
-  float wango=22020303.0f;
-  for (int i=0; i<100'000; i++)
-  {
-    wango = sqrtf(wango);
-  }
-  // Call yerself
-  // if(left != right)
-  // quicksort(numbers,left,pivot-1);
-  // quicksort(numbers,pivot+1,right);
-  return true;
+// Use the quicksort algorithm to sort ints given:
+// 1. The address of a basic c array in memory
+// 2. The left index of the section of array we're sorting
+// 3. The right index of the section.
+bool quicksortInts(int* numbers, int left, int right);
+
+// Sort a basic c array of integers provided:
+// 1. the start of the array and 2. the number of ints.
+void sortIntsAscending(int* integers, int size);
+void displayArray(int* theArray, int length);
+
+int main()
+{
+  // Make an array of ints
+  const int LIST_LENGTH = 17;
+  int randomInts[LIST_LENGTH]{13, 4, 20, 15, 13, 9, 2, 17, 1, 24, 22, 8, 14, 5, 13, 5, 7};
+
+  cout << "\n";
+  displayArray(randomInts, LIST_LENGTH);
+  cout << "\n";
+
+  dmac::Timer timer;
+  timer.start();
+  sortIntsAscending(randomInts, LIST_LENGTH);
+  timer.stop();
+  
+  cout << "\n";
+  displayArray(randomInts, LIST_LENGTH);
+  cout << "\n";
+
+  cout << "\n\t-----------------------------------------\n";
+  cout << "\t#######   quicksort time: " << timer.timeTakenMilli() << " milliseconds   ######\n";
+  cout << "\t-----------------------------------------\n";
+
+  //cout << "Hi bro, nice qs" << endl;
+}
+
+void sortIntsAscending(int* integers, int size)
+{
+	bool success = quicksortInts(integers, 0, size - 1);
+
+}
+
+bool quicksortInts(int* numbers, int left, int right)
+{
+	// If our left and right indicies haven't finally collided 
+	int pivot = 0;
+	int pivotIndex = 0;
+	if (left < right)
+	{
+		// Split the collection, sort the left and right sides.
+		pivotIndex = choosePivotAndPartition(numbers, left, right);
+		// Call quicksort on segment left of the pivot
+		quicksortInts(numbers, left, pivotIndex - 1);
+		// Do the sam on right.
+		quicksortInts(numbers, pivotIndex + 1, right);
+	}
+
+	return true;
+}
+
+int choosePivotAndPartition(int* numbers, int left, int right)
+{
+	// Choose the left most number as the pivot.
+	// The left wall, the last known place we moved a lower-than-pivot number.
+	// Starts out as 
+	int pivotValue = numbers[left];
+	int leftWall = left;
+
+	for (int i = leftWall + 1; i <= right; i++)
+	{
+		// Beginning at a point right of the wall, check if the number there
+		// is less than the pivot. if so swap them and move the left wall forward,
+		// collecting smaller numbers in safety.
+		// The pivot will be pushed out ahead on the first swap to position wall+1
+		if (numbers[i] < pivotValue)
+		{
+			// save the tasty small number
+			int subPivotValue = numbers[i];
+			// swap it whatever is right of the wall.
+			numbers[i] = numbers[leftWall + 1];
+			numbers[leftWall + 1] = subPivotValue;
+			leftWall += 1;	// Move the wall to gobble up the nice low value.
+		} // else left wall stands still while i marches on looking for a new lower than pivot value.
+
+	}
+	// Finally the left wall is our last found smaller-than-pivot value, and
+	// everything to the right of it is pivot sized or higher. Our first value though,
+	// at numbers[low] still holds the pivot value. Swap the two and the left wall spot
+	// now holds the pivot (followed by values equal to or higher than it), and numbers[low]
+	// holds a smaller value
+	int subPivotValue = numbers[leftWall];
+	numbers[left] = subPivotValue;
+	numbers[leftWall] = pivotValue;
+	return leftWall;
 }
 
 void displayArray(int* theArray, int length)
@@ -38,30 +116,4 @@ void displayArray(int* theArray, int length)
 		if (i < length - 1) cout << ",";
 	}
 	cout << "]\n";
-}
-
-int main()
-{
-  // Make an array of ints
-  const int LIST_LENGTH = 13;
-  int sortMe[LIST_LENGTH]{20, 4, 1, 15, 9, 2, 24, 22, 8, 14, 5, 5, 7};
-
-  cout << "\n";
-  displayArray(sortMe, LIST_LENGTH);
-  cout << "\n";
-
-  dmac::Timer timer;
-  timer.start();
-  quicksort(sortMe, 0, LIST_LENGTH);
-  timer.stop();
-  
-  cout << "\n";
-  displayArray(sortMe, LIST_LENGTH);
-  cout << "\n";
-
-  cout << "\n\t-----------------------------------------\n";
-  cout << "\t#######   quicksort time: " << timer.timeTakenMilli() << " milliseconds   ######\n";
-  cout << "\t-----------------------------------------\n";
-
-  //cout << "Hi bro, nice qs" << endl;
 }
--- quicksort_multi_d/main.cpp
+++ quicksort_multi_d/main.cpp
@@ -10,9 +10,25 @@ using namespace std;
 int loadRandsFile(uint16_t* destination, string fileName);
 bool quickSort(uint16_t* toSort, int leftIndex, int rightIndex, int pivot);
 
+// Selects a pivot int in the provided range and places 
+// lower ints to left, higher ints to right.
+int choosePivotAndPartition(uint16_t* numbers, int left, int right);
+
+// Use the quicksort algorithm to sort ints given:
+// 1. A c array of unsigned 16 bit ints.
+// 2. The left index of the section of array we're sorting
+// 3. The right index of the section.
+bool quicksortInts(uint16_t* numbers, int left, int right);
+
+// Sort a basic c array of integers provided:
+// 1. the start of the array and 2. the number of ints.
+void sortIntsAscending(uint16_t* integers, int size);
+
+bool writeRandsFile(uint16_t* randsArray, int randCount, string filename);
+
 int main()
 {
-    int listCount = 6;
+    int listCount = 8;
     int listLength = 100'000;
     uint16_t** randlists = new uint16_t * [listCount];
 
@@ -30,35 +46,18 @@ int main()
     timer.start();
     for (int i = 0; i < listCount; i++)
     {
-        quickSort(randlists[i], 0, listLength - 1, listLength - 1);
+        //quickSort(randlists[i], 0, listLength - 1, listLength - 1);
+        sortIntsAscending(randlists[i], listLength);
     }
     timer.stop();
 
     cout << "\n\t TIME TAKEN\n";
     cout << "\t ------------------\n";
     cout << "\t " << timer.timeTakenMilli() << " milliseconds \n";
+ //   writeRandsFile(randlists[0], listLength, "sorted_rands_1.txt");
     return 0;
 }
 
-bool quickSort(uint16_t* toSort, int leftIndex, int rightIndex, int pivot)
-{
-    cout << "\t *sorting* \n";
-    /*
-    cout << "\t first 10: ";
-    
-    for (int i = 0; i < 10; i++)
-    {
-        cout << toSort[i] << " ";
-    }
-    cout << "\n";
-    */
-    for (int i = 0; i <= rightIndex; i++)
-    {
-        // Square root each number to use up some clock.
-        toSort[i] = static_cast<uint16_t>(std::round(std::sqrt(toSort[i])));
-    }
-    return true;
-}
 
 int loadRandsFile(uint16_t* destination, string fileName)
 {
@@ -73,4 +72,74 @@ int loadRandsFile(uint16_t* destination, string fileName)
     }
     randsFile.close();
     return lineCount;
+}
+
+bool writeRandsFile(uint16_t* randsArray, int randCount, string fileName)
+{
+    ofstream randsFile;
+    randsFile.open(fileName, ios::out);
+    for (int i = 0; i < randCount -1; i++)
+    {
+        randsFile << randsArray[i] << endl;
+    }
+    randsFile.close();
+    return true;
+}
+
+void sortIntsAscending(uint16_t* integers, int size)
+{
+    bool success = quicksortInts(integers, 0, size - 1);
+}
+
+bool quicksortInts(uint16_t* numbers, int left, int right)
+{
+    // If our left and right indicies haven't finally collided 
+    int pivotIndex = 0;
+    if (left < right)
+    {
+        // Split the collection, sort the left and right sides.
+        pivotIndex = choosePivotAndPartition(numbers, left, right);
+        // Call quicksort on segment left of the pivot
+        quicksortInts(numbers, left, pivotIndex - 1);
+        // Do the sam on right.
+        quicksortInts(numbers, pivotIndex + 1, right);
+    }
+
+    return true;
+}
+
+int choosePivotAndPartition(uint16_t* numbers, int left, int right)
+{
+    // Choose the left most number as the pivot.
+    // The left wall, the last known place we moved a lower-than-pivot number.
+    // Starts out as 
+    auto pivotValue = numbers[left];
+    int leftWall = left;
+
+    for (int i = leftWall + 1; i <= right; i++)
+    {
+        // Beginning at a point right of the wall, check if the number there
+        // is less than the pivot. if so swap them and move the left wall forward,
+        // collecting smaller numbers in safety.
+        // The pivot will be pushed out ahead on the first swap to position wall+1
+        if (numbers[i] < pivotValue)
+        {
+            // save the tasty small number
+            auto subPivotValue = numbers[i];
+            // swap it whatever is right of the wall.
+            numbers[i] = numbers[leftWall + 1];
+            numbers[leftWall + 1] = subPivotValue;
+            leftWall += 1;	// Move the wall to gobble up the nice low value.
+        } // else left wall stands still while i marches on looking for a new lower than pivot value.
+
+    }
+    // Finally the left wall is our last found smaller-than-pivot value, and
+    // everything to the right of it is pivot sized or higher. Our first value though,
+    // at numbers[low] still holds the pivot value. Swap the two and the left wall spot
+    // now holds the pivot (followed by values equal to or higher than it), and numbers[low]
+    // holds a smaller value
+    auto subPivotValue = numbers[leftWall];
+    numbers[left] = subPivotValue;
+    numbers[leftWall] = pivotValue;
+    return leftWall;
 }
